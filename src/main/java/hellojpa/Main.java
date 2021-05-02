68ac7907ca20f7f7b52728f82bddd3c2364dad34
kgw012 <kgw012@gmail.com>
2021-05-02 11:44:18 +0900

    210501 / jpa practice complete

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -1,11 +1,14 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import hellojpa.entity.Member;
+import hellojpa.entity.Team;
 
 public class Main {
 
@@ -18,13 +21,39 @@ public class Main {
 		tx.begin();
 		
 		try {
-			Member member = new Member();
-			member.setId(100L);
-			member.setName("안녕하세요");
 			
+			Team team = new Team();
+			team.setName("TeamA");
+			em.persist(team);
+			
+			Member member = new Member();
+			member.setName("hello");
 			em.persist(member);
+			
+			team.getMembers().add(member);
+			member.setTeam(team);
+			
+			em.flush();
+			em.clear();
+			
+			String jpql = "select m from Member m join fetch m.team";
+			
+			List<Member> members = em.createQuery(jpql, Member.class)
+					.setFirstResult(10)
+					.setMaxResults(20)
+					.getResultList();
+			
+			em.close();
+			
+			for(Member member1 : members) {
+				System.out.println("username = " + member1.getName());
+				System.out.println("teamname = " + member1.getTeam().getName());
+			}
+			
+			
 			tx.commit();
 		} catch(Exception e) {
+			e.printStackTrace();
 			tx.rollback();
 		} finally {
 			em.close();
--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -1,25 +1,48 @@
 package hellojpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Member {
 	
-	@Id
+	@Id @GeneratedValue
 	private Long id;
+	
+	@Column(name = "USERNAME", nullable = false, length = 20)
 	private String name;
 	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
+	
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 }
